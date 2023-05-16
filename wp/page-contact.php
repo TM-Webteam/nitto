f9c350b856b135bf49d7559dd556ba60bdf7d0ed
tmkato <kato@tmedia.co.jp>
2023-05-16 21:45:23 +0900

    フォ－ム　追加

---
+++ wp/page-contact.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+
+<main class="contact">
+
+  <section class="hero03 core">
+    <div class="containers">
+      <div class="hero03__box">
+        <h1 class="ttl-primary-lower">お問合せ・ご相談</h1>
+        <div class="lead">日東エルマテリアル製品に関するお問合せ・ご相談はお気軽にご連絡ください。</div>
+      </div>
+    </div>
+  </section>
+
+  <section class="form">
+    <div class="containers">
+      <div class="form__note">
+        <p>● 以下フォームにご連絡先、お問合せ内容をご記入の上、送信してください。担当よりご連絡申し上げます。</p>
+        <p>● 回答までにお時間をいただく場合や、回答ができない場合もございますので、ご了承ください。</p>
+      </div>
+
+      <div id="satori__creative_container">
+        <script id="-_-satori_creative-_-" src="//delivery.satr.jp/js/creative_set.js" data-key="159f4a1420dee4c3"></script>
+      </div>
+      
+    </div>
+  </section>
+
+</main>
+
+<?php get_footer(); ?>
